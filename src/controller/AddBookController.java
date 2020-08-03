99de0ffed9ca2f5730ea81acdb1022d97858342d
Waruna lakshan <warulakshan96@gmail.com>
2020-08-03 15:49:57 +0530

    finish Add Books Controller class

--- src/controller/AddBookController.java
+++ src/controller/AddBookController.java
@@ -1,7 +1,114 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class AddBookController {
     public AnchorPane root;
+    public JFXTextField txt_Name;
+    public JFXTextField txt_Author;
+    public JFXTextField txt_Quantity;
+    public JFXTextField txt_ISBN;
+    public JFXButton btn_Add_new;
+    public Label lbl_BookId;
+    public JFXButton btn_Save;
+
+    public void btn_Add_new_OnAction(ActionEvent actionEvent) {
+        try{
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select book_id from books order by book_id desc limit 1");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()){
+                lbl_BookId.setText("B001");
+            }else{
+                String lastCode = resultSet.getString(1);
+                String substring = lastCode.substring(1,4);
+                int newCode = Integer.parseInt(substring) + 1;
+                if (newCode < 10){
+                    lbl_BookId.setText("B00" + newCode);
+                }else if (newCode < 100){
+                    lbl_BookId.setText("B0" + newCode);
+                }else {
+                    lbl_BookId.setText("B" + newCode);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void txt_ISBN_OnAction(ActionEvent actionEvent) {
+        saveBook();
+    }
+
+    public void btn_Save_OnAction(ActionEvent actionEvent) {
+        saveBook();
+    }
+
+    private void saveBook(){
+
+        if (lbl_BookId.getText().equals("Book ID")){
+            new Alert(Alert.AlertType.ERROR, "press Add New Book Button", ButtonType.OK).show();
+            btn_Add_new.requestFocus();
+            return;
+        }
+        if (txt_Name.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, " No name included", ButtonType.OK).show();
+            txt_Name.requestFocus();
+            return;
+        }
+        if (txt_Author.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "please enter author name", ButtonType.OK).show();
+            txt_Author.requestFocus();
+            return;
+        }
+        if (txt_Quantity.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "enter quantity", ButtonType.OK).show();
+            txt_Quantity.requestFocus();
+            return;
+        }
+        if (txt_ISBN.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "enter ISBN", ButtonType.OK).show();
+            txt_ISBN.requestFocus();
+            return;
+        }
+
+        String id = lbl_BookId.getText();
+        String name = txt_Name.getText();
+        String author = txt_Author.getText();
+        int qty = Integer.parseInt(txt_Quantity.getText());
+        String isbn = txt_ISBN.getText();
+
+        try{
+            PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement("INSERT INTO books VALUES (?,?,?,?,?)");
+            preparedStatement.setObject(1, id);
+            preparedStatement.setObject(2,name);
+            preparedStatement.setObject(3, author);
+            preparedStatement.setObject(4,qty);
+            preparedStatement.setObject(5,isbn);
+            preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        new Alert(Alert.AlertType.INFORMATION, "Successfully added..", ButtonType.OK).show();
+        txt_Name.clear();
+        txt_Author.clear();
+        txt_ISBN.clear();
+        txt_Quantity.clear();
+        btn_Add_new.requestFocus();
+    }
+
 }
--- src/controller/AddMemberController.java
+++ src/controller/AddMemberController.java
@@ -1,8 +1,132 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AddMemberController {
 
     public AnchorPane root;
+    public JFXTextField txt_name;
+    public JFXTextField txt_NIC;
+    public JFXTextField txt_Address;
+    public JFXTextField txt_contact;
+    public JFXButton btn_AddNewMember;
+    public Label lbl_MemberId;
+    public JFXButton btn_Save;
+
+    public void btn_AddNewMember_OnAction(ActionEvent actionEvent) {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select member_id from members order by member_id desc limit 1");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                lbl_MemberId.setText("M001");
+            } else {
+                String lastCode = resultSet.getString(1);
+                String substring = lastCode.substring(1, 4);
+                int newCode = Integer.parseInt(substring) + 1;
+                if (newCode < 10) {
+                    lbl_MemberId.setText("M00" + newCode);
+                } else if (newCode < 100) {
+                    lbl_MemberId.setText("M0" + newCode);
+                } else {
+                    lbl_MemberId.setText("M" + newCode);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void btn_Save_OnAction(ActionEvent actionEvent) {
+
+        if (lbl_MemberId.getText().equals("Member ID")){
+            new Alert(Alert.AlertType.ERROR, "Please press Add New member Button and generate member ID..", ButtonType.OK).show();
+            btn_AddNewMember.requestFocus();
+            return;
+        }
+        if (txt_name.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Enter name please", ButtonType.OK).show();
+            txt_name.requestFocus();
+            return;
+        }
+        if (txt_NIC.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Enter NIC no please", ButtonType.OK).show();
+            txt_NIC.requestFocus();
+            return;
+        }
+        if (txt_Address.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Enter address please", ButtonType.OK).show();
+            txt_Address.requestFocus();
+            return;
+        }
+        if (txt_contact.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Contact No enter Please", ButtonType.OK).show();
+            txt_contact.requestFocus();
+            return;
+        }
+
+        String id = lbl_MemberId.getText();
+        String name = txt_name.getText();
+        String NIC = txt_NIC.getText();
+        String address = txt_Address.getText();
+        String contact = txt_contact.getText();
+
+        if (btn_Save.getText().equals("SAVE")) {
+
+            try {
+                Connection connection = DBConnection.getInstance().getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO members values (?,?,?,?,?) ");
+                preparedStatement.setObject(1, id);
+                preparedStatement.setObject(2, name);
+                preparedStatement.setObject(3, NIC);
+                preparedStatement.setObject(4, address);
+                preparedStatement.setObject(5, contact);
+                preparedStatement.executeUpdate();
+                new Alert(Alert.AlertType.INFORMATION, "Successfully added...", ButtonType.OK).show();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        txt_name.clear();
+        txt_NIC.clear();
+        txt_contact.clear();
+        txt_Address.clear();
+//        System.exit(0);
+
+    }
 }
+
+
+        /*else {
+            try {
+                PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement("UPDATE members set name=?, NIC=?, address=?, contact=? where id=?");
+                preparedStatement.setObject(1,name);
+                preparedStatement.setObject(2,NIC);
+                preparedStatement.setObject(3,address);
+                preparedStatement.setObject(4,contact);
+                preparedStatement.setObject(5,id);
+                preparedStatement.executeUpdate();
+
+                new Alert(Alert.AlertType.INFORMATION, "Update Finish", ButtonType.OK).show();
+
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }*/
+
